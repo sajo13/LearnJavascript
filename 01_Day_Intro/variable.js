d69161a72fa6033fcfbd1ca3807ce75703f98c35
Sajo-007 <sajo.sunny@prophaze.com>
2025-09-02 09:27:36 +0000

    Variables can also be declaring in one line separated by comma

--- 01_Day_Intro/variable.js
+++ 01_Day_Intro/variable.js
@@ -12,3 +12,9 @@ let isMarried = false
 const gravity = 9.81         // earth gravity  in m/s2
 const boilingPoint = 100     // water boiling point, temperature in oC
 const PI = 3.14              // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = 'Asabeneh',     //name of a person
+  job = 'teacher',
+  live = 'Finland'
