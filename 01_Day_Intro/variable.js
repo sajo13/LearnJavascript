eacbe15e8621a41919b7db03d55ca399f83289c8
Sajo-007 <sajo.sunny@prophaze.com>
2025-09-02 09:27:14 +0000

    Declaring variables with number values

--- 01_Day_Intro/variable.js
+++ 01_Day_Intro/variable.js
@@ -6,3 +6,9 @@ let country = 'India'       // country
 let city = 'Kottayam'         // capital city
 let age = 25                 // age in years
 let isMarried = false
+
+// Declaring variables with number values
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, temperature in oC
+const PI = 3.14              // geometrical constant
