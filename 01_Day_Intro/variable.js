5ef4b49a01c63338c9fb80f0d7b0e43a6c6173ea
Sajo-007 <sajo.sunny@prophaze.com>
2025-09-02 09:26:28 +0000

    Declaring different variables of different data types

---
+++ 01_Day_Intro/variable.js
@@ -0,0 +1,8 @@
+// Declaring different variables of different data types
+
+let firstName = 'Sajo'    // first name of a person
+let lastName = 'Sunny'      // last name of a person
+let country = 'India'       // country
+let city = 'Kottayam'         // capital city
+let age = 25                 // age in years
+let isMarried = false
